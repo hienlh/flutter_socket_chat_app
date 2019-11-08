c613712e3a21edb07a0c4891a70f9d2e20c7f499
hienlh <hienlh1298@gmail.com>
2019-11-08 13:52:38 +0700

    Complete Chat UI

--- lib/chatScreen.dart
+++ lib/chatScreen.dart
@@ -1,16 +1,50 @@
+import 'package:dash_chat/dash_chat.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  List<ChatMessage> chatMessages = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Chat Screen'),
-        ),
+      appBar: AppBar(
+        title: Text('Resman User Chat'),
+      ),
+      body: DashChat(
+        user: ChatUser(name: 'Hieren Lee'),
+        messages: <ChatMessage>[
+          ChatMessage(
+            user: ChatUser(
+              name: 'abcde',
+              avatar:
+                  'https://www.wrappixel.com/ampleadmin/assets/images/users/4.jpg',
+            ),
+            text: 'abcd',
+          ),
+          ChatMessage(
+            user: ChatUser(uid: 'abcde', name: 'abcde'),
+            text: 'abcd',
+          ),
+          ChatMessage(
+            user: ChatUser(uid: 'abcde', name: 'abcde'),
+            text: 'abcd',
+          ),
+          ChatMessage(
+            user: ChatUser(uid: 'abcde', name: 'abcde'),
+            text: 'abcd',
+          ),
+        ],
+        onSend: (ChatMessage chatMessage) {
+          print(chatMessage.text);
+        },
+        showAvatarForEveryMessage: false,
       ),
     );
   }
-
 }
